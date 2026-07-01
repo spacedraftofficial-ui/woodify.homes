a4693b4f5658e797e7b1bee086e036cdbe852703
spacedraftofficial <spacedraftofficial@gmail.com>
2026-07-01 15:11:06 +0530

    Auto-update: 2026-07-01 15:11:06

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { HiX, HiOutlineLocationMarker, HiOutlineCalendar, HiOutlineHashtag, HiChevronRight, HiChevronLeft } from 'react-icons/hi';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
 import { portfolioData, type Project } from '../../data/portfolio';
 
 export const Portfolio: React.FC = () => {
