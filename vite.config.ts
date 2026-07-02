afac16bddcaf10bcdaac80c6eca44f9f5a372ce9
spacedraftofficial <spacedraftofficial@gmail.com>
2026-07-02 12:03:01 +0530

    Auto-update: 2026-07-02 12:03:01

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // test comment
 export default defineConfig({
-  base: '/woodify.homes/',
+  base: '/',
   plugins: [react()],
 })
