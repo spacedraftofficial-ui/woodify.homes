b7f72b0b03a3c7022ea1b4842dc9ab444a262a06
spacedraftofficial <spacedraftofficial@gmail.com>
2026-07-02 11:06:06 +0530

    Auto-update: 2026-07-02 11:06:06

--- src/components/Services/Spaces.tsx
+++ src/components/Services/Spaces.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-// @ts-ignore
+// @ts-expect-error
 import LightGallery from 'lightgallery/react';
-// @ts-ignore
+// @ts-expect-error
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
-// @ts-ignore
+// @ts-expect-error
 import lgZoom from 'lightgallery/plugins/zoom';
 
 interface SpaceType {
