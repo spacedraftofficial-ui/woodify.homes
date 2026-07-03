bdc7d5474bb0a0ae64ea79f3d601341523160d55
spacedraftofficial <spacedraftofficial@gmail.com>
2026-07-03 16:06:18 +0530

    fix: change base path to relative ./ to support subfolder deployments like GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// test comment
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [react()],
 })
+
